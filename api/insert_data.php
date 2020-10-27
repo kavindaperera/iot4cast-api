aab7dde740365a09ca5b2c9ea0f057957cd4cef5
kavindaperera-arimac <kavinda.perera@arimacdigital.com>
2020-10-28 02:07:01 +0530

    changed file names

--- api/insert_data.php
+++ api/insert_data.php
@@ -27,7 +27,7 @@ if( $_POST["xml"]) {
  
     if ($result) {
         $response["success"] = 1;
-        $response["message"] = "Weather successfully created.";
+        $response["message"] = "Weather record successfully created.";
         echo json_encode($response);
     } else {
         $response["success"] = 0;
